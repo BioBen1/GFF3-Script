4fb09458d10c1f85fbf58f2f9c2257a9c4f7225c
BioBen1 <b.burrows1987@gmail.com>
2016-07-19 10:24:22 -0700

    Filter to Problem File
    Added a filter to pull anything that was between 85473 and 107422 into a new file name problem.
    This will work for parent features but there is a problem that the children of those parents may not fall within this range so could be left in the file.  
    Hopefully the GFF3 viewer would not allow these....
    Might be able to work in something to deal with them later, but right now beyond me.

--- gff3parse.pl
+++ gff3parse.pl
@@ -6,7 +6,10 @@ open ( $INPUT, "<", "TEST.gff3")
 	or die "Can't open < TEST.gff3: $!";
 
 open ( $OUTPUT, ">", "output.txt")
-	or die "Can't open > output.txt: $!";	
+	or die "Can't open > output.txt: $!";
+
+open ( $PROBLEM, ">", "problem.txt")
+	or die "Can't open > problem.txt: $!";	
 
 $n = 0;
 
@@ -21,6 +24,11 @@ while ( $line = <$INPUT> ){
 		# Should print anything that was before the changes as the same bp location
 		if ($L[4] < 85473 ){
 			print { $OUTPUT } $line;
+		} else {
+			# Test if the feature was in the region where new sequence was added. (Not sure I should keep using else each time, maybe could use another if?)
+			if ( ( 85473 <= $L[3] && $L[3] <= 107422 ) || ( 85473 <= $L[4] && $L[4] <= 107422 ) ){
+				print {$PROBLEM} $line;
+			}
 		}
 	}
 }
@@ -29,3 +37,4 @@ close $INPUT;
 
 close $OUTPUT;
 
+close $PROBLEM;
